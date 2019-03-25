225c4d052e1d28ab9340ff8157d4c20e48d04208
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-03-25 21:44:03 +0100

    Add key in horse definition

--- apis/src/horse_helper.py
+++ apis/src/horse_helper.py
@@ -21,34 +21,42 @@ def write_in_horses_file(data):
 
 def _horse_format():
     return [{
+            'key': 'name',
             'name': 'Nom',
             'description': 'Le nom du cheval',
             'type': 'string'
         },{
+            'key': 'sire',
             'name': 'N° SIRE',
             'description': 'Le numéro de SIRE du cheval',
             'type': 'string'
         },{
+            'key': 'transponder',
             'name': 'N° Transpondeur',
             'description': 'Le numéro de transpondeur du cheval',
             'type': 'string'
         },{
+            'key': 'owner',
             'name': 'Propriétaire',
             'description': 'Nom et coordonnées du propriétaire',
             'type': 'string'
         },{
+            'key': 'incoming_at',
             'name': 'Entrée',
             'description': 'Date de première entré dans le centre',
             'type': 'date'
         },{
+            'key': 'incoming_from',
             'name': 'Provenance',
             'description': "Adresse de provenance de l'animal",
             'type': 'string'
         },{
+            'key': 'outgoing_at',
             'name': 'Sortie',
             'description': 'Date de sortie définitive du centre',
             'type': 'date'
         },{
+            'key': 'outgoing_to',
             'name': 'Destination',
             'description': "Adresse de destination de l'animal lors de la sortie définitive du centre",
             'type': 'string'
