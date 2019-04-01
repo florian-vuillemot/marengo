08a3a14b089ce587970e6499d3a9845dc296bd1d
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-04-01 21:17:55 +0200

    Add healthcare module

---
+++ front/marengo/src/lib/Healthcare.js
@@ -0,0 +1,9 @@
+import GenericData from './GenericData';
+
+class Healthcare extends GenericData {
+    constructor(props){
+        super('healthcares', props);
+    }
+}
+
+export default Healthcare;
