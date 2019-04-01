057e95592941f23df14ac95411252c5753772409
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-04-01 15:26:18 +0200

    Add image for select render

--- front/marengo/src/App.js
+++ front/marengo/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import GenericTable from './lib/GenericTable';
 import './App.css';
 import Horses from './lib/Horses';
+import horsesPicture from './images/horses.jpg';
+import horseTransport from './images/transport.jpg';
 
 
 const GenericTableWrapper = ({fields, values, updateValue, saveValue, cancelValue, removeValue}) =>
@@ -14,14 +16,44 @@ const GenericTableWrapper = ({fields, values, updateValue, saveValue, cancelValu
     removeValue={removeValue}
   />
 
-function App(){
-  return (
-    <div className="App">
-      <div className="App-body">
-        <Horses table={GenericTableWrapper} />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      view: null
+    };
+
+    this.horsesView = this.horsesView.bind(this);
+    this.selectRender = this.selectRender.bind(this);
+  }
+
+  showRender = () => this.setState({view: null});
+
+  horsesView = () => <Horses table={GenericTableWrapper} cb={this.showRender}/>;
+
+  selectRender() {
+    return (
+      <ul className="App-select">
+        <li onClick={() => this.setState({view: this.horsesView})}>
+          <img className="Select-image" src={horsesPicture} alt="Accèder au chevaux" />
+        </li>
+        <li>
+          <img className="Select-image" src={horseTransport} alt="Accèder au mouvement des chevaux" />
+        </li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-body">
+          {!this.state.view ? this.selectRender() : this.state.view()}
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default App;
