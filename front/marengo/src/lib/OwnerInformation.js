3d90f04408766f95e36c66a9cd104f262dde5a40
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-04-04 07:43:07 +0200

    Add owner information in module in front

---
+++ front/marengo/src/lib/OwnerInformation.js
@@ -0,0 +1,9 @@
+import GenericData from './GenericData';
+
+class OwnerInformation extends GenericData {
+    constructor(props){
+        super('owner_information', props);
+    }
+}
+
+export default OwnerInformation;
