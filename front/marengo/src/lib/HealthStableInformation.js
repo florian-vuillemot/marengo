69a54919b377e7eb2a39297a0ff37c79e527b492
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-04-14 16:14:44 +0200

    (front) Add health stable information

---
+++ front/marengo/src/lib/HealthStableInformation.js
@@ -0,0 +1,9 @@
+import GenericData from './GenericData';
+
+class HealthStableInformation extends GenericData {
+    constructor(props){
+        super('health_stable_information', props);
+    }
+}
+
+export default HealthStableInformation;
